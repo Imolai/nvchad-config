82b0f06d037153e8bdeca6cda49757af314d460a
Gábor <38439709+Imolai@users.noreply.github.com>
2023-11-27 11:55:13 +0100

    Add current line and column in status line

--- chadrc.lua
+++ chadrc.lua
@@ -7,6 +7,13 @@ local highlights = require "custom.highlights"
 M.ui = {
   theme = "onedark",
   theme_toggle = { "onedark", "one_light" },
+  statusline = {
+    -- https://github.com/NvChad/NvChad/issues/1562
+    -- modules arg here is the default table of modules
+    overriden_modules = function(modules)
+      modules[11] = " %l,%c "
+    end,
+  },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
